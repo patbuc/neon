dc600333d803e87033c84551bb31114039b87c0c
Patric Bucher <patricbucher@gmail.com>
2023-10-29 20:23:55 +0100

    Created the rust project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # neon
-A toy language nobody was waiting for
+Hi, this is <span style="color:rgb(240,0,255)">**neon**</span> - a toy language you didn't wait for.
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use colored::Colorize;
+
+fn main() {
+    println!(
+        "Hi, this is {} - a toy language you didn't wait for.",
+        "neon".truecolor(240,0,255).bold()
+    );
+}
