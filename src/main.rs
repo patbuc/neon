61ecff861fa13c2026ffb281bb0e439a6f633619
Patric Bucher <patricbucher@gmail.com>
2023-10-30 23:07:25 +0100

    Adding the Block
    A block is a blob of vm opcodes that can be run by the neon VM.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+mod block;
+
 use colored::Colorize;
+use crate::block::{Block, OpCode};
+
+#[cfg(feature = "disassemble")]
+use crate::block::{BlockDbg};
 
 fn main() {
     println!(
         "Hi, this is {} - a toy language you didn't wait for.",
         "neon".truecolor(240,0,255).bold()
     );
+
+    let mut block = Block::new("ZeBlock");
+    block.push_op_code(OpCode::Return);
+
+    #[cfg(feature = "disassemble")]
+    block.disassemble_block();
 }
+
+
