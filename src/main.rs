7bb4b15e7183be291d7224f971f27ee7fc418c5b
Patric Bucher <patricbucher@gmail.com>
2023-10-30 23:14:08 +0100

    Applying Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod block;
 
-use colored::Colorize;
 use crate::block::{Block, OpCode};
+use colored::Colorize;
 
 #[cfg(feature = "disassemble")]
-use crate::block::{BlockDbg};
+use crate::block::BlockDbg;
 
 fn main() {
     println!(
         "Hi, this is {} - a toy language you didn't wait for.",
-        "neon".truecolor(240,0,255).bold()
+        "neon".truecolor(240, 0, 255).bold()
     );
 
     let mut block = Block::new("ZeBlock");
@@ -18,5 +18,3 @@ fn main() {
     #[cfg(feature = "disassemble")]
     block.disassemble_block();
 }
-
-
