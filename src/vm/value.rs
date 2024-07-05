7459a4978d3ff087499dbed915ec76e4959e087d
Patric Bucher <patricbucher@gmail.com>
2024-07-05 18:40:09 +0100

    Replacing Value printing with a Display implementation

--- src/vm/value.rs
+++ src/vm/value.rs
@@ -1,4 +1,20 @@
 use crate::vm::Value;
+use std::fmt::{Display, Formatter};
+
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Value::Number(val) => val.to_string(),
+                Value::Boolean(val) => val.to_string(),
+                Value::String(val) => val.to_string(),
+                Value::Nil => "nil".to_string(),
+            }
+        )
+    }
+}
 
 #[macro_export]
 macro_rules! number {
