fc8165a4ec79fc03181504695319bf82563f471f
Patric Bucher <patricbucher@gmail.com>
2024-07-15 22:01:40 +0200

    Moving parser code out of the mod.rs

--- src/compiler/parser/emitter.rs
+++ src/compiler/parser/emitter.rs
@@ -3,26 +3,30 @@ use crate::vm::opcodes::OpCode;
 use crate::vm::{Block, Value};
 
 impl Parser {
-    pub(super) fn emit_return(&mut self) {
+    fn current_block(&mut self) -> &mut Block {
+        self.blocks.last_mut().unwrap()
+    }
+
+    pub fn emit_return(&mut self) {
         self.emit_op_code(OpCode::Return);
     }
 
-    pub(super) fn emit_constant(&mut self, value: Value) {
+    pub fn emit_constant(&mut self, value: Value) {
         let line = self.previous_token.line;
         self.current_block().write_constant(value, line)
     }
 
-    pub(super) fn emit_string(&mut self, value: Value) {
+    pub fn emit_string(&mut self, value: Value) {
         let line = self.previous_token.line;
         self.current_block().write_string(value, line)
     }
 
-    pub(super) fn emit_op_code(&mut self, op_code: OpCode) {
+    pub fn emit_op_code(&mut self, op_code: OpCode) {
         let line = self.previous_token.line;
         self.current_block().write_op_code(op_code, line);
     }
 
-    pub(super) fn emit_op_codes(&mut self, op_code1: OpCode, op_code2: OpCode) {
+    pub fn emit_op_codes(&mut self, op_code1: OpCode, op_code2: OpCode) {
         let line = self.previous_token.line;
         let current_block: &mut Block = self.current_block();
         current_block.write_op_code(op_code1, line);
