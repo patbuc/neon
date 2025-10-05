ead77148b815ac3cf45098d4a5f3946c5d0436b4
Patric Bucher <patricbucher@gmail.com>
2025-10-05 15:39:25 +0200

    replace current_brick_mut method with macro usage
    - introduce current_brick_mut! macro for accessing last mutable brick
    - update parser emitter methods to use macro instead of method calls
    - remove redundant current_brick and current_brick_mut methods from Parser

--- src/compiler/parser/emitter.rs
+++ src/compiler/parser/emitter.rs
@@ -1,16 +1,9 @@
 use crate::common::opcodes::OpCode;
 use crate::common::{Brick, Local, Value};
 use crate::compiler::Parser;
+use crate::current_brick_mut;
 
 impl Parser {
-    pub(super) fn current_brick(&self) -> &Brick {
-        self.bricks.last().unwrap()
-    }
-
-    fn current_brick_mut(&mut self) -> &mut Brick {
-        self.bricks.last_mut().unwrap()
-    }
-
     pub fn emit_return(&mut self) {
         self.emit_op_code(OpCode::Return);
     }
@@ -18,59 +11,56 @@ impl Parser {
     pub fn emit_constant(&mut self, value: Value) -> u32 {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        self.current_brick_mut().write_constant(value, line, column)
+        current_brick_mut!(self.bricks).write_constant(value, line, column)
     }
 
     pub fn define_value(&mut self, name: String) {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
         let depth = self.scope_depth;
-        self.current_brick_mut()
-            .define_value(Local::new(name, depth), line, column)
+        current_brick_mut!(self.bricks).define_value(Local::new(name, depth), line, column)
     }
 
     pub fn define_variable(&mut self, name: String) {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
         let depth = self.scope_depth;
-        self.current_brick_mut()
-            .define_variable(Local::new(name, depth), line, column)
+
+        current_brick_mut!(self.bricks).define_variable(Local::new(name, depth), line, column)
     }
 
     pub fn emit_string(&mut self, value: Value) -> u32 {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        self.current_brick_mut().write_string(value, line, column)
+        current_brick_mut!(self.bricks).write_string(value, line, column)
     }
 
     pub fn emit_op_code(&mut self, op_code: OpCode) {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        self.current_brick_mut()
-            .write_op_code(op_code, line, column);
+        current_brick_mut!(self.bricks).write_op_code(op_code, line, column);
     }
 
     pub fn emit_op_code_variant(&mut self, op_code: OpCode, index: u32) {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        self.current_brick_mut()
-            .write_op_code_variant(op_code, index, line, column);
+        current_brick_mut!(self.bricks).write_op_code_variant(op_code, index, line, column);
     }
 
     pub fn emit_jump(&mut self, op_code: OpCode) -> u32 {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        self.current_brick_mut().emit_jump(op_code, line, column)
+        current_brick_mut!(self.bricks).emit_jump(op_code, line, column)
     }
 
     pub fn patch_jump(&mut self, offset: u32) {
-        self.current_brick_mut().patch_jump(offset);
+        current_brick_mut!(self.bricks).patch_jump(offset);
     }
 
     pub fn emit_op_codes(&mut self, op_code1: OpCode, op_code2: OpCode) {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        let current_brick: &mut Brick = self.current_brick_mut();
+        let current_brick: &mut Brick = current_brick_mut!(self.bricks);
         current_brick.write_op_code(op_code1, line, column);
         current_brick.write_op_code(op_code2, line, column);
     }
@@ -78,6 +68,6 @@ impl Parser {
     pub fn emit_loop(&mut self, loop_start: u32) {
         let line = self.previous_token.line;
         let column = self.previous_token.column;
-        self.current_brick_mut().emit_loop(loop_start, line, column);
+        current_brick_mut!(self.bricks).emit_loop(loop_start, line, column);
     }
 }
--- src/compiler/parser/mod.rs
+++ src/compiler/parser/mod.rs
@@ -2,3 +2,10 @@ mod emitter;
 mod parser;
 mod rules;
 mod scanner;
+
+#[macro_export]
+macro_rules! current_brick_mut {
+    ($bricks:expr) => {
+        $bricks.last_mut().unwrap()
+    };
+}
