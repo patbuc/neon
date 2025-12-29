ea07fc9d24cdeb2346257895f2343cdd0afa3a26
Patric Bucher <patricbucher@gmail.com>
2025-12-29 17:21:01 +0100

    Remove obsolete test and example files

--- test_semantic_args.rs
+++
@@ -1,7 +0,0 @@
-use neon::compiler::semantic::SemanticAnalyzer;
-
-fn main() {
-    let analyzer = SemanticAnalyzer::new();
-    // Check if args is defined
-    println!("SemanticAnalyzer created");
-}
