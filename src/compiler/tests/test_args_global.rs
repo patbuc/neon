f41b49282587d13023300b1df66edddc5393d8e5
Patric Bucher <patricbucher@gmail.com>
2026-01-02 16:44:15 +0100

    Clean up method registry and finalize binary compilation removal
    Renames get_native_method to get_native_method_by_name for clarity.
    Consolidates callable representation by replacing Object::Callable
    with Object::NativeFunction and removes CallableKind enum.
    Updates codegen to use unified emit_native_call_by_index and
    emit_native_call_by_name helpers for cleaner call generation.

--- src/compiler/tests/test_args_global.rs
+++ src/compiler/tests/test_args_global.rs
@@ -29,4 +29,72 @@ mod test_args_global {
 
         assert!(result.is_ok(), "args should be accessible");
     }
+
+    #[test]
+    fn asdf() {
+        let program = r#"
+        val test = [
+"987654321111111",
+"811111111111119",
+"234234234234278",
+"818181911112111",
+]
+
+val f = File("/Users/patricbucher/Develop/aoc-neon/aoc31.txt")
+val input = f.readLines()
+val lines = input[0].split(",")
+
+fn reduce(line, remove) {
+    // Build characters in an array, then join once
+    var chars = []
+    val n = line.len()
+    for (var i = 0; i < n; i++) {
+        if (remove.contains(i)) {
+            continue
+        }
+        chars.push(line.charAt(i))
+    }
+    return chars.join("")
+}
+
+var password = 0
+for (line in input) {
+
+    var the_line = line
+    while (the_line.len() > 12) {
+        val remove = []
+        for (var j = 0; j < the_line.len() - 1; j++) {
+            val a = the_line.charAt(j).toInt()
+            val b = the_line.charAt(j + 1).toInt()
+            if (a < b) {
+                remove.push(j)
+                break
+            }
+        }
+        if (remove.size() == 0) {
+            remove.push(the_line.len() - 1)
+        }
+        the_line = reduce(the_line, remove)
+    }
+
+    password = password + the_line.toInt()
+    // print(the_line)
+}
+
+print("Password: " + password.toString())
+"#;
+        let mut vm = crate::vm::VirtualMachine::new();
+        match vm.interpret(program.to_string()) {
+            crate::vm::Result::Ok => {
+                println!("Output:\n{}", vm.get_output());
+            }
+            crate::vm::Result::CompileError => {
+                let formatted_errors = vm.get_formatted_errors("test.neon");
+                panic!("Compilation failed:\n{}", formatted_errors);
+            }
+            crate::vm::Result::RuntimeError => {
+                panic!("Runtime error:\n{}", vm.get_output());
+            }
+        }
+    }
 }
