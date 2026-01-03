f8538884c2958545abb2eabdbd409daf4f3d75be
Patric Bucher <patricbucher@gmail.com>
2026-01-03 13:31:40 +0100

    Removed leftovers from chunk compilation removal

--- src/common/chunk/binary/format.rs
+++
@@ -1,5 +0,0 @@
-/// Magic number for Neon binary format: ASCII "NEON"
-pub const MAGIC_NUMBER: [u8; 4] = [0x4E, 0x45, 0x4F, 0x4E];
-
-/// Current binary format version
-pub const FORMAT_VERSION: u16 = 1;
--- src/common/chunk/mod.rs
+++ src/common/chunk/mod.rs
@@ -3,7 +3,5 @@ mod r#impl;
 
 #[cfg(feature = "disassemble")]
 mod disassembler;
-
-pub mod binary;
 #[cfg(test)]
 mod tests;
